731f57d5d358c185fe4c41c0056433b8057746d3
josefinemsson <josmar@kth.se>
2015-03-31 11:41:10 +0200

    get things from API

--- app/js/mainCtrl.js
+++ app/js/mainCtrl.js
@@ -1,8 +1,8 @@
 tasteMeApp.controller('MainCtrl', function ($scope,Model) {
 
+	$scope.status= "Add some things you like to the left and get recommendations!";
 
 	$scope.getLikeList = function (){
-		console.log('Gettar likelist')
 		return Model.getLikeList();
 	}
 
@@ -16,4 +16,18 @@ tasteMeApp.controller('MainCtrl', function ($scope,Model) {
 	}
 
 
+	$scope.createNewRecList = function() {
+
+   		Model.likeSearch.get({q:Model.getStringLikeList()},function(data){
+
+   		var allData = data.Similar;
+   		var ourData = allData.Results;
+   		$scope.getRecList = ourData;
+   		console.log(data);
+   		$scope.status = "";
+   },function(data){
+     $scope.status = "There was an error";
+   });
+ }
+	
 });
--- app/js/model.js
+++ app/js/model.js
@@ -1,33 +1,44 @@
-tasteMeApp.factory('Model',function ($resource) {
+tasteMeApp.factory('Model',function ($resource,$http) {
 
-	var likeList= ['Harry Potter','Finding Nemo'];
-	console.log('Inne i model')
-
-this.getLikeList = function(){
-	console.log(likeList)
-	return likeList;
-}
-
- this.addToLikeList = function(likedItem)
- {	
- 	console.log(likeList)
- 	likeList.push(likedItem)
- }
+	var likeList= [];
+	var recList = [];
 
 
- this.removeFromLikeList = function (name)
- {
- 	for (i=0;i<likeList.length;i++)
- 	{
- 		if (likeList[i]===name)
- 		{
- 			likeList.splice(i,1);
- 		}
- 	}
- }
+	this.getStringLikeList = function(){
+		var string = ''
+		for(var i = 0; i<likeList.length ; i++){
+			string += likeList[i]+',';
+		}
+		string = string.substring(0,string.length-1);
+		console.log(string)
+		return string;
+	}
 
- 
+	this.likeSearch = $resource('http://www.tastekid.com/api/similar?k=76627-TasteMe-RG4042W1',{limit:10,callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
 
- return this;
 
+	this.getLikeList = function(){
+		return likeList;
+	}
+
+
+	this.addToLikeList = function(likedItem){	
+	 	likeList.push(likedItem)
+	 	console.log(likeList);
+	}
+
+
+	this.removeFromLikeList = function (name){
+	 	for (i=0;i<likeList.length;i++){
+	 		if (likeList[i]===name){
+	 			likeList.splice(i,1);
+	 		}
+	 	}
+	}
+
+	this.getRecList = function (){
+		return recList
+	}
+	
+	return this;
 });
